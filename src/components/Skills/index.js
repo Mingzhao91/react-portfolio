f46761552a9e94f95c64659d70d10f54f0ba0c18
Mingzhao91 <edwardwhite7@hotmail.com>
2023-03-11 13:48:19 +0000

    create skills component

---
+++ src/components/Skills/index.js
@@ -0,0 +1,24 @@
+// import libraries
+import React from "react";
+
+// import styles
+import "./style.css";
+
+function Skills() {
+  return (
+    <div className="skills">
+      <div className="skill bubble"></div>
+      <div className="skill servoy"></div>
+      <div className="skill bootstrap"></div>
+      <div className="skill css3"></div>
+      <div className="skill github"></div>
+      <div className="skill html"></div>
+      <div className="skill javascript"></div>
+      <div className="skill jquery"></div>
+      <div className="skill nodejs"></div>
+      <div className="skill react"></div>
+    </div>
+  );
+}
+
+export default Skills;
