a0bd5a8b84d1497e272ae11ff553078be564755d
Mingzhao91 <edwardwhite7@hotmail.com>
2023-03-11 22:49:26 +0000

    create contact item component

---
+++ src/components/ContactItem/index.js
@@ -0,0 +1,35 @@
+//import libraries
+import React from "react";
+
+// import style
+import "./style.css";
+
+function ContactItem(props) {
+  return (
+    <div className="mb-3">
+      <p className="contact-details-title">{props.title}</p>
+      <div className="contact-details-list">
+        <ul className="contact-details-list">
+          {props.items
+            ? props.items.map((item) => {
+                return (
+                  <li key={item.linkUrl} className="contact-item mb-1">
+                    <a
+                      className="text-light contact-link"
+                      href={item.linkUrl}
+                      rel="noreferrer"
+                      target="_blank"
+                    >
+                      {item.linkName}
+                    </a>
+                  </li>
+                );
+              })
+            : ""}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default ContactItem;
