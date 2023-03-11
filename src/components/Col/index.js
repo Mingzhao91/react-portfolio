4592572b587ac14985bda7316a32b30e811220c9
Mingzhao91 <edwardwhite7@hotmail.com>
2023-03-11 15:09:44 +0000

    Col accepts classes array

--- src/components/Col/index.js
+++ src/components/Col/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 
 function Col(props) {
-  const size = props.size.split(" ").map(size => "col-" + size).join(" ");
+  const size = props.size
+    ? props.size
+        .split(" ")
+        .map((size) => "col-" + size)
+        .join(" ")
+    : "";
 
-  return <div className={size}>{props.children}</div>;
+  const classes = props.classes ? props.classes.join(" ") : "";
+
+  return <div className={`${size} ${classes}`}>{props.children}</div>;
 }
 
 export default Col;
