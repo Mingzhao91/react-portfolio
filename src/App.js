b4c1dcb939d9806d312a3132e3c6d14a0b0005c0
Mingzhao91 <edwardwhite7@hotmail.com>
2023-03-11 12:34:52 +0000

    fix the path for getting the home component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // import pages
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
