bef75a7a161ec78b2cae304896579855aec5cdc1
Mingzhao91 <edwardwhite7@hotmail.com>
2023-03-12 20:36:33 +0000

    create project component to display individual project

---
+++ src/components/Project/index.js
@@ -0,0 +1,60 @@
+// import libraries
+import React from "react";
+
+// import style
+import "./style.css";
+
+// "id": 1,
+// "name": "Sightseeing Guide(Team Project)",
+// "image": "./assets/projectsScreenshots/sightseeingGuide.png",
+// "description": "Our team has developed a comprehensive, user-friendly website that integrates both Wikipedia and Google APIs to provide a wealth of geographic and historical information to users. The website is designed to serve as an interactive platform for exploring various locations and gaining insights into their history and significance.",
+// "repoUrl": "https://github.com/macaulam/sightseeing-guide",
+// "appUrl": "https://macaulam.github.io/sightseeing-guide/"
+
+function Project(props) {
+  return (
+    <div className="d-flex flex-wrap justify-content-between  p-4 project-wrapper">
+      <div
+        className={
+          parseInt(props.id) % 2 === 0
+            ? "order-1 panel-left d-flex justify-content-center align-items-center"
+            : "order-0 panel-left d-flex justify-content-center align-items-center"
+        }
+      >
+        <div className="project-img-wrapper">
+          <img className="project-img" src={props.image} alt={props.name}></img>
+        </div>
+      </div>
+      <div
+        className={
+          parseInt(props.id) % 2 === 0
+            ? "order-0 panel-right d-flex flex-column justify-content-center align-items-center"
+            : "order-1 panel-right d-flex flex-column justify-content-center align-items-center"
+        }
+      >
+        <p className="mb-2 project-name">{props.name}</p>
+        <p className="mb-3 project-description">{props.description}</p>
+        <div className="buttons">
+          <a
+            href={props.appUrl}
+            target="_blank"
+            rel="noreferrer"
+            className="btn project-btn me-3"
+          >
+            Deployed Application
+          </a>
+          <a
+            href={props.appUrl}
+            target="_blank"
+            rel="noreferrer"
+            className="btn project-btn"
+          >
+            GitHub Repository
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,11 +1,39 @@
 // import libraries
-import React from "react";
+import React, { useState } from "react";
+
+// import projects
+import projectsJSON from "../../projects.json";
+
+// import components
+import Wrapper from "../../components/Wrapper";
+import Project from "../../components/Project";
 
 // import style
 import "./style.css";
 
 function Projects() {
-  return <div>this is projects page</div>;
+  const [projects] = useState([...projectsJSON]);
+
+  return (
+    <Wrapper>
+      <div className="projects-page mt-5 mx-auto">
+        <h2 className="projects-title text-center py-4">Selected Project</h2>
+        {projects.map((project) => (
+          <div className="projects-wrapper">
+            <Project
+              id={project.id}
+              key={project.id}
+              name={project.name}
+              image={project.image}
+              description={project.description}
+              repoUrl={project.repoUrl}
+              appUrl={project.appUrl}
+            />
+          </div>
+        ))}
+      </div>
+    </Wrapper>
+  );
 }
 
 export default Projects;
